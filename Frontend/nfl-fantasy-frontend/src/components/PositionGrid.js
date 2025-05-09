e51394ebfa764e298b7f2fe1394a37cac718cf17
russellchao <russchaoman@gmail.com>
2025-05-08 22:19:31 -0400

    Basic layout of positions page

---
+++ Frontend/nfl-fantasy-frontend/src/components/PositionGrid.js
@@ -0,0 +1,40 @@
+import React, { use } from 'react'; 
+import { useNavigate } from 'react-router-dom';
+
+const positionNames = [
+    "Quarterback", "Running Back", "Wide Receiver", "Tight End", "Kicker"
+];
+
+
+const PositionGrid = () => {
+    const navigate = useNavigate(); 
+
+    const handleTeamClick = (teamName) => {
+        // navigate(`/teams/${teamName}`); 
+    };
+
+    return (
+        <div style={{
+            display: 'grid', 
+            gridTemplateColumns: 'repeat(5, 1fr)',
+            gap: '10px',
+            padding: '20px'
+        }}>
+            {positionNames.map(position => (
+                <div key={position}
+                    onClick={() => handleTeamClick(position)}
+                    style={{
+                        padding: '10px',
+                        backgroundColor: '#ddd',
+                        textAlign: 'center',
+                        cursor: 'pointer'
+                    }}>
+                    {position}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+
+export default PositionGrid; 
--- Frontend/nfl-fantasy-frontend/src/pages/Positions.js
+++ Frontend/nfl-fantasy-frontend/src/pages/Positions.js
@@ -1,9 +1,11 @@
 import React from 'react'; 
+import PositionGrid from '../components/PositionGrid';
 
 const Positions = () => {
     return (
         <div>
             <h1 style={{ paddingLeft: '20px' }}>Select a Position</h1>
+            <PositionGrid />
         </div>
     );
 };
