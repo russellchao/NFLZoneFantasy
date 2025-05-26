477d2e8381d3360101188976ebae75225d6af530
russellchao <russchaoman@gmail.com>
2025-05-26 13:15:37 -0400

    Properly extracting matchup names for any season, any week.

--- Backend/DataScraping/GameData/GetGameData.py
+++ Backend/DataScraping/GameData/GetGameData.py
@@ -34,7 +34,6 @@ def getGameData2025Week1():
         for matchup in gamesThisDate.get("games"):
             print(matchup.get("name"))
 
-
 def getGameData2024Week15():
 
     #EXAMPLE: Extracting the Week 15 Schedule for the 2024-25 NFL Season using an unofficial ESPN API
@@ -64,24 +63,52 @@ def getGameData2024Week15():
 
     
         
+def get_schedule_data(year, week, seasonType):
+
+    espn_api_url = f"https://cdn.espn.com/core/nfl/schedule?xhr=1&year={year}&week={week}&seasontype={seasonType}"
+
+    response = requests.get(espn_api_url)
+
+    if (response.status_code != 200):
+        print(int(response.headers["Retry-After"]))
+        raise Exception(f"Failed to load ESPN page ({response.status_code})")
+    
+    data = response.json()
+    content = data.get("content", {})
+    schedule = content.get("schedule", {})
+
+    with open("ScheduleOutput.json", "w") as file:
+        json.dump(schedule, file, indent=4)
 
 
+    # Testing Output
+    if seasonType == 2:
+        print(f"Week {week} {year} Schedule dates:\n")
+    elif seasonType == 3:
+        seasonKeys = {"1": "Wild Card Round", "2": "Divisional Round", "3": "Conference Championships", "5": "Super Bowl"}
+        print(f"{seasonKeys.get(str(week))} {year} dates:\n")
+
+    for date in schedule:
+        # each "date" follows the format of something like: YYYYMMDD (e.g. 20250904)
+        print(f"Date: {date}")
+
+        gamesThisDate = schedule[date]
+        for matchup in gamesThisDate.get("games"):
+            print(matchup.get("name"))
+
+        print()
 
 
 
     
 
     
-    
-    
-
-
-
-
-
-
 if __name__ == "__main__":
 
-    # Test extracting the schedule
-    getGameData2025Week1()
-    getGameData2024Week15()
+    # Test examples extracting the schedule (for seasonType: 1=preseason, 2=regular season, 3=playoffs) but idc about preseason
+    # e.g. 
+    #   get_schedule_data(2025, 1, 2) for Week 1 of 2025, 
+    #   get_schedule_data(2024, 15, 2) for Week 15 of 2024, 
+    #   get_schedule_data(2024, 5, 3) for Super Bowl of 2024-25 (Eagles 40, Chiefs 22)
+
+    get_schedule_data(year=2024, week=5, seasonType=3)
