ffdcc4e4decd4bd94e8f6cd2f78308ad65ea87e5
russellchao <russchaoman@gmail.com>
2025-05-26 12:40:04 -0400

    Able to properly retrieve matchup names for a given week and season

--- Backend/DataScraping/GameData/GetGameData.py
+++ Backend/DataScraping/GameData/GetGameData.py
@@ -7,43 +7,66 @@ import time
 import csv
 
 
-def get_game_data_test():
+def getGameData2025Week1():
 
     #EXAMPLE: Extracting the Week 1 Schedule for the 2025-26 NFL Season using an unofficial ESPN API
     espn_api_url = "https://cdn.espn.com/core/nfl/schedule?xhr=1&year=2025&week=1"
 
     response = requests.get(espn_api_url)
 
-    if (response.status_code == 200):
-        data = response.json()
-        content = data.get("content", {})
-        schedule = content.get("schedule", {})
-
-        with open("Week1_2025.json", "w") as file:
-           json.dump(schedule, file, indent=4)
-
-    else:
+    if (response.status_code != 200):
         print(int(response.headers["Retry-After"]))
         raise Exception(f"Failed to load ESPN page ({response.status_code})")
+    
+    data = response.json()
+    content = data.get("content", {})
+    schedule = content.get("schedule", {})
+
+    with open("Week1_2025.json", "w") as file:
+        json.dump(schedule, file, indent=4)
+
+    print("Week 1 2025 Schedule dates:")
+    for date in schedule:
+        # each "date" follows the format of something like: YYYYMMDD (e.g. 20250904)
+        print(f"Date: {date}")
+
+        gamesThisDate = schedule[date]
+        for matchup in gamesThisDate.get("games"):
+            print(matchup.get("name"))
 
 
+def getGameData2024Week15():
 
     #EXAMPLE: Extracting the Week 15 Schedule for the 2024-25 NFL Season using an unofficial ESPN API
     espn_api_url = "https://cdn.espn.com/core/nfl/schedule?xhr=1&year=2024&week=15"
 
     response = requests.get(espn_api_url)
 
-    if (response.status_code == 200):
-        data = response.json()
-        content = data.get("content", {})
-        schedule = content.get("schedule", {})
-
-        with open("Week15_2024.json", "w") as file:
-           json.dump(schedule, file, indent=4)
-
-    else:
+    if (response.status_code != 200):
         print(int(response.headers["Retry-After"]))
         raise Exception(f"Failed to load ESPN page ({response.status_code})")
+
+    data = response.json()
+    content = data.get("content", {})
+    schedule = content.get("schedule", {})
+
+    with open("Week15_2024.json", "w") as file:
+        json.dump(schedule, file, indent=4)
+
+    print("Week 15 2024 Schedule dates:")
+    for date in schedule:
+        # each "date" follows the format of something like: YYYYMMDD (e.g. 20250904)
+        print(f"Date: {date}")
+
+        gamesThisDate = schedule[date]
+        for matchup in gamesThisDate.get("games"):
+            print(matchup.get("name"))
+
+    
+        
+
+
+
 
 
     
@@ -58,8 +81,7 @@ def get_game_data_test():
 
 
 if __name__ == "__main__":
-    
-    print("Game retriever file")
 
     # Test extracting the schedule
-    get_game_data_test()
+    getGameData2025Week1()
+    getGameData2024Week15()
