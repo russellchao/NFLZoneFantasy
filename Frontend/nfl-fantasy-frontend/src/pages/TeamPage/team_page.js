cbdf3a7eb5ab1722768c37c5704721212abd7b20
russellchao <russchaoman@gmail.com>
2025-06-05 21:47:18 -0400

    Can now efficiently navigate between schedule, player stats, and roster on each team's page. Considering adding a team stats section.

--- Frontend/nfl-fantasy-frontend/src/pages/TeamPage/team_page.js
+++ Frontend/nfl-fantasy-frontend/src/pages/TeamPage/team_page.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchPlayerDataByTeam, fetchUpdatePlayerStatsDB } from '../../API/player_data_api';
 import { fetchScheduleByTeam, fetchUpdateScheduleDB } from '../../API/schedule_api';
@@ -13,11 +13,14 @@ const TeamPage = () => {
     const allSections = ["Schedule", "Player Stats", "Roster"];
     const [teamSeason, setSeason] = useState("2025"); 
     const [loading, setLoading] = useState(true);
-    const [loadError, throwLoadError] = useState(false); 
+
+    // Track if the initial fetch has been done
+    const initialFetchRef = useRef(false);
 
 
     // For schedule section
     const [schedule, setSchedule] = useState([]); 
+    const [scheduleError, setScheduleError] = useState(false); 
 
 
     // For player stats section
@@ -26,9 +29,11 @@ const TeamPage = () => {
     const [receivers, setReceivers] = useState([]); 
     const [defenders, setDefenders] = useState([]); 
     const [kickers, setKickers] = useState([]); 
+    const [playerStatsError, setPlayerStatsError] = useState(false); 
      
   
     // For roster section
+    const [rosterError, setRosterError] = useState(false); 
 
 
 
@@ -38,16 +43,19 @@ const TeamPage = () => {
     };
 
 
-    // Call the Specialized Spring Boot endpoint to update the specified CSV and database table
+    // Functions to call the Spring Boot endpoints that update the specified CSV and database table
     async function updatePlayerStatsDB() {
         if (!loading) {
             setLoading(true); 
         }
 
-        // In case updating the player stats CSV is unsuccessful, the function will return such a response
-        const csv_result = await fetchUpdatePlayerStatsDB(teamSeason); 
+        const response = await fetchUpdatePlayerStatsDB(teamSeason); 
 
-        if (csv_result === "Failure updating CSVs") { throwLoadError(true); }
+        if (response === "Failure updating CSVs") {
+            setPlayerStatsError(true); 
+            setLoading(false); 
+            return; 
+        }
     };
 
     async function updateScheduleDB() {
@@ -55,96 +63,102 @@ const TeamPage = () => {
             setLoading(true); 
         }
 
-        // In case updating the schedule CSV is unsuccessful, the function will return such a response
-        const csv_result = await fetchUpdateScheduleDB(teamSeason); 
+        const response = await fetchUpdateScheduleDB(teamSeason); 
 
-        if (csv_result === "Failure updating CSVs") { throwLoadError(true); }
+        if (response === "Failure updating CSVs") {
+            setScheduleError(true); 
+            setLoading(false); 
+            return; 
+        }
     };
 
 
 
 
-
-
-
-
     useEffect(() => {
-        throwLoadError(false); // reset loadError to false on each load
+        setPlayerStatsError(false);
+        setScheduleError(false);
 
-        // Update the section based on the based on the current path
-        if (section === "Schedule") {
-            (async () => {
-                /*** 
-                 * Placing this logic on an async wrapper allows us to wait for the database 
-                 * to completely update before fetching the data
-                 * */ 
-                await updateScheduleDB(); 
-                console.log(`Retreiving schedule from ${teamSeason}`);
+        // Only update the databases on initial load or if the season changes
+        if (initialFetchRef.current) {
+            return; 
+        } else {
+            initialFetchRef.current = true; 
+        }
 
-                // Retrieve schedule from the Spring Boot Backend (for schedule section)
-                if (teamName) {
-                    const loadSchedule = async () => {
-                        const scheduleData = await fetchScheduleByTeam(teamName); 
-                        setSchedule(scheduleData); 
-                    }
-                    loadSchedule();
+        (async () => {
+            // Update the schedule database
+            await updateScheduleDB(); 
 
-                    setLoading(false); 
+            // Update the player stats database
+            await updatePlayerStatsDB(); 
 
-                    console.log(schedule);
+            // Update the roster database
+            // NOTE: Roster data is not currently being fetched from the Spring Boot Backend, so this function is not implemented
+    
+
+
+            // Retrieve schedule from the Spring Boot Backend
+            console.log(`Retreiving schedule from ${teamSeason}`);
+            if (teamName) {
+                const loadSchedule = async () => {
+                    const scheduleData = await fetchScheduleByTeam(teamName); 
+                    setSchedule(scheduleData); 
+                }
+                loadSchedule();
+
+                setLoading(false); 
+
+                console.log(schedule);
+            };
+                
+
+
+            // Retrieve player data from the Spring Boot Backend (for player stats section)
+            console.log(`Retreiving player stats from ${teamSeason}`); 
+            if (teamName) {
+                // Fetch Passing data
+                const loadPassers = async () => {
+                    const passingData = await fetchPlayerDataByTeam("passer", teamName); 
+                    setPassers(passingData); 
                 };
-            })();
+                loadPassers(); 
 
-
-        } else if (section === "Player Stats") {
-            (async () => { 
-                await updatePlayerStatsDB(); 
-                console.log(`Retreiving player stats from ${teamSeason}`)
-
-                // Retrieve player data from the Spring Boot Backend (for player stats section)
-                if (teamName) {
-                    // Fetch Passing data
-                    const loadPassers = async () => {
-                        const passingData = await fetchPlayerDataByTeam("passer", teamName); 
-                        setPassers(passingData); 
-                    };
-                    loadPassers(); 
-
-                    // Fetch Rushing data
-                    const loadRushers = async () => {
-                        const rushingData = await fetchPlayerDataByTeam("rusher", teamName); 
-                        setRushers(rushingData); 
-                    };
-                    loadRushers(); 
-
-                    // Fetch Receiving data
-                    const loadReceivers = async () => {
-                        const receivingData = await fetchPlayerDataByTeam("receiver", teamName); 
-                        setReceivers(receivingData); 
-                    };
-                    loadReceivers(); 
-
-                    // Fetch Defense data
-                    const loadDefenders = async () => {
-                        const defenseData = await fetchPlayerDataByTeam("defender", teamName); 
-                        setDefenders(defenseData); 
-                    };
-                    loadDefenders(); 
-
-                    // Fetch Kicking data
-                    const loadKickers = async () => {
-                        const kickingData = await fetchPlayerDataByTeam("kicker", teamName); 
-                        setKickers(kickingData); 
-                    };
-                    loadKickers(); 
-
-
-                    setLoading(false); 
+                // Fetch Rushing data
+                const loadRushers = async () => {
+                    const rushingData = await fetchPlayerDataByTeam("rusher", teamName); 
+                    setRushers(rushingData); 
                 };
-            })(); 
-        };
+                loadRushers(); 
 
-    }, [teamName, teamSeason, section]);
+                // Fetch Receiving data
+                const loadReceivers = async () => {
+                    const receivingData = await fetchPlayerDataByTeam("receiver", teamName); 
+                    setReceivers(receivingData); 
+                };
+                loadReceivers(); 
+
+                // Fetch Defense data
+                const loadDefenders = async () => {
+                    const defenseData = await fetchPlayerDataByTeam("defender", teamName); 
+                    setDefenders(defenseData); 
+                };
+                loadDefenders(); 
+
+                // Fetch Kicking data
+                const loadKickers = async () => {
+                    const kickingData = await fetchPlayerDataByTeam("kicker", teamName); 
+                    setKickers(kickingData); 
+                };
+                loadKickers(); 
+
+
+                setLoading(false); 
+            }; 
+
+        })();
+
+    }, [teamSeason, teamName]);
 
 
     // When the page is loading data
@@ -154,49 +168,6 @@ const TeamPage = () => {
         );
     }
 
-
-    console.log("Load error: ", loadError);
-
-
-    // When the player stats cannot be fetched
-    if (loadError) {
-        return (
-            <div>
-                <p style={{ paddingLeft: '20px' }}>
-                    Error, could not load {section} for the {teamName}' {teamSeason} season.
-                </p>
-
-                {/* Menu for each section */}
-                <div style={{
-                    display: 'grid', 
-                    gridTemplateColumns: 'repeat(3, 1fr)',
-                    gap: '10px',
-                    padding: '20px',
-                    width: '75%'
-                }}>
-                    {allSections.map(thisSection => (
-                        <div key={thisSection}
-                            onClick={() => handleSectionChange(thisSection)}
-                            style={{
-                                padding: '10px',
-                                backgroundColor: '#ddd',
-                                textAlign: 'center',
-                                cursor: 'pointer'
-                            }}>
-                            {thisSection}
-                        </div>
-                    ))}
-                </div>
-
-                {/* Season section drop-down menu */}
-                <SeasonDropdownMenu
-                    teamSeason = {teamSeason}
-                    onChange = {setSeason}
-                />
-
-            </div>
-        );
-    }
 
 
     return (
@@ -229,28 +200,43 @@ const TeamPage = () => {
             {/* Season section drop-down menu */}
             <SeasonDropdownMenu
                 teamSeason = {teamSeason}
-                onChange = {setSeason}
+                onChange={(newSeason) => {
+                    setSeason(newSeason);
+                    initialFetchRef.current = false;
+                }}
             />
             
             <p>&nbsp;</p>
             
 
             {section === "Schedule" ? (
-                <Schedule 
-                    schedule = {schedule}
-                />
+                <>
+                    {scheduleError ? (
+                        <p style={{ paddingLeft: '20px' }}>Error, could not load the schedule for the {teamSeason} season.</p>
+                    ) : (
+                        <Schedule 
+                            schedule = {schedule}
+                        /> 
+                    )};
+                </>
 
             ) : section === "Roster" ? (
                 <h2 style={{ paddingLeft: '20px' }}>Roster</h2>
 
             ) : section === "Player Stats" ? (
-                <PlayerStats 
-                    passers = {passers}
-                    rushers = {rushers}
-                    receivers = {receivers}
-                    defenders = {defenders}
-                    kickers = {kickers}
-                />
+                <>
+                    {playerStatsError ? (
+                        <p style={{ paddingLeft: '20px' }}>Error, could not load the player stats for the {teamSeason} season.</p>
+                    ) : (
+                        <PlayerStats 
+                            passers = {passers}
+                            rushers = {rushers}
+                            receivers = {receivers}
+                            defenders = {defenders}
+                            kickers = {kickers}
+                        />
+                    )};
+                </>
 
             ) : null}
 
