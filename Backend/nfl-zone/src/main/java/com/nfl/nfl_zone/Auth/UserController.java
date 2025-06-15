d31277316246b1ecbc2e4736f00aace966afc9a6
russellchao <russchaoman@gmail.com>
2025-06-15 16:35:47 -0400

    Removed 'final' keyword from UserController

--- Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserController.java
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(final UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
