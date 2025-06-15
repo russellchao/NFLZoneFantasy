7aae6392c3e92e1f9bea9038a698925b7fab3e0d
russellchao <russchaoman@gmail.com>
2025-06-15 16:04:12 -0400

    Installed spring-boot-starter-security in pom.xml and working on User Service.

---
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserService.java
@@ -0,0 +1,11 @@
+package com.nfl.nfl_zone.Auth;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+
+}
