1feeaa090012880f8e33aa0537ad5aa6c35a4eae
russellchao <russchaoman@gmail.com>
2025-06-15 15:54:32 -0400

    Created User Repository

---
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nfl.nfl_zone.Auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
