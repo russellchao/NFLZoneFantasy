208829620b71ceeaf9dcba90e3df052e0a5aa412
russellchao <russchaoman@gmail.com>
2025-06-15 16:11:25 -0400

    Implemented User Service.

--- Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserService.java
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/Auth/UserService.java
@@ -6,6 +6,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return "Username already exists";
+        }
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return "Email already exists";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
 
 }
