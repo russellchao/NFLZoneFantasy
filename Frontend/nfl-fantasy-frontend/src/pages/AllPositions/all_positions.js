7423a27fcd253c7c48c80f94e22b8c8b0cd06d18
russellchao <russchaoman@gmail.com>
2025-06-20 11:53:59 -0400

    Corrected typo in all positions page.

--- Frontend/nfl-fantasy-frontend/src/pages/AllPositions/all_positions.js
+++ Frontend/nfl-fantasy-frontend/src/pages/AllPositions/all_positions.js
@@ -6,7 +6,7 @@ const AllPositions = () => {
         <div>
             <h1 style={{ paddingLeft: '20px' }}>Select a Position</h1>
             <p style={{ paddingLeft: '20px' }}>
-                NOTE: For defense (and special teams) in fatasy football, you draft an entire team.
+                NOTE: For defense (and special teams) in fantasy football, you draft an entire team.
             </p>
             <p>&nbsp;</p>
             <PositionGrid />
