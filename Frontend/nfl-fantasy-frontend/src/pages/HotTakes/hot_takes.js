51546aa17b1f2535dd80247a679b2e7fd93d4ef1
russellchao <russchaoman@gmail.com>
2025-07-05 16:11:02 -0400

    Added more rules to the hot takes.

--- Frontend/nfl-fantasy-frontend/src/pages/HotTakes/hot_takes.js
+++ Frontend/nfl-fantasy-frontend/src/pages/HotTakes/hot_takes.js
@@ -2,17 +2,21 @@ import React, {useState, useEffect} from 'react';
 
 const HotTakes = () => {
 
+    const currentSeason = "2025-26";
+
     return (
         <div style={{ padding: "20px" }}>
             <>
                 <h1>Hot Takes</h1>
                 <h3>This is where you can place your hottest (or not so hot) takes for the upcoming NFL season. </h3>
+                <h3 style={{ color:'blue' }}>Current Season: {currentSeason}</h3>
             </>
 
             <br></br>
 
             <>
                 <h2>Enter your hot takes here</h2>
+                <h4 style={{ color:'maroon' }}>IMPORTANT: Hot takes are meant to be placed before the regular season and will no longer be accepted upon the start of the regular season (Sep 4, 2025).</h4>
             </>
 
             <br></br>
@@ -20,9 +24,9 @@ const HotTakes = () => {
             <>
                 <h4>Rules</h4>
                 <p>1. You may place up to 10 hot takes.</p>
-                <p>2. Hot takes are meant to be placed before the start of the Regular Season. HOT TAKES WILL NO LONGER BE ACCEPTED AFTERWARDS!</p>
-                <p>3. Hot takes must be specific and measurable. For example, "The Buffalo Bills will win the Super Bowl" is a valid hot take, while "The Bills will be good" is not.</p>
-                <p>4. Hot takes that involve predicting injuries and suspensions will not be accepted.</p>
+                <p>2. Hot takes must be specific and measurable. For example, "The Buffalo Bills will win the Super Bowl" is a valid hot take, while "The Bills will be good" is not.</p>
+                <p>3. Hot takes that involve predicting injuries and suspensions will not be accepted.</p>
+                <p>4. Hot takes that are too similar to other hot takes won't be accepted. Diversity matters!</p>
                 <p>5. Each hot take will be evaluated by an NLP Model to determine its validity.</p>
                 <p>6. You will be awarded User Points based on the accuracy of your hot take mesaused by an NLP Model following the Postseason.</p>
             </>
