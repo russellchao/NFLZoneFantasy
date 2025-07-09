1846a6395038e80c124f71a4aeb5c510edc306a7
russellchao <russchaoman@gmail.com>
2025-07-08 21:11:19 -0400

    HotTakeService.java can successfully add hot takes to the database.

--- Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/HotTakes/HotTakeService.java
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/HotTakes/HotTakeService.java
@@ -7,6 +7,12 @@ import java.util.*;
 @Service
 public class HotTakeService {
 
+    private final HotTakeRepository hotTakeRepository;
+
+    public HotTakeService(HotTakeRepository hotTakeRepository) {
+        this.hotTakeRepository = hotTakeRepository;
+    }
+
     public String validateHotTake(String hotTake) {
         String flaskURL = String.format("http://localhost:5000/validateHotTake/%s", hotTake);
         RestTemplate restTemplate = new RestTemplate();
@@ -27,6 +33,25 @@ public class HotTakeService {
     }
 
     public String saveHotTake(String username, String hotTake) {
+
+        // Check if the username exists in the hot_takes table
+        if (hotTakeRepository.findByUsername(username).isEmpty()) {
+            System.out.println("User \"" + username + "\" does not exist in hot_takes table. Creating new user...");
+
+            // Add a new user to the hot_takes table if it doesn't exist
+            HotTakes newUser = new HotTakes();
+            newUser.setUsername(username);
+            newUser.setHotTakes(new ArrayList<>());
+            hotTakeRepository.save(newUser);
+        }
+
+        // If the given user has less than 10 hot takes, add the new hot take to the list
+        if (hotTakeRepository.findByUsername(username).get().getHotTakes().size() < 10) {
+            hotTakeRepository.findByUsername(username).get().getHotTakes().add(hotTake);
+            hotTakeRepository.save(hotTakeRepository.findByUsername(username).get());
+        } else {
+            return "Spring Boot: User \"" + username + "\" has reached the maximum number of hot takes (10)";
+        }
 
         return "Spring Boot: Successfully saved hot take \"" + hotTake + "\" for user \"" + username + "\"";
     }
--- Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/HotTakes/HotTakes.java
+++ Backend/nfl-zone/src/main/java/com/nfl/nfl_zone/HotTakes/HotTakes.java
@@ -1,24 +1,15 @@
 package com.nfl.nfl_zone.HotTakes;
 
 import jakarta.persistence.*;
+import java.util.*;
 
 @Entity
 @Table(name = "hot_takes")
 public class HotTakes {
 
     @Id
-    public String username;
-
-    public String hotTake1;
-    public String hotTake2;
-    public String hotTake3;
-    public String hotTake4;
-    public String hotTake5;
-    public String hotTake6;
-    public String hotTake7;
-    public String hotTake8;
-    public String hotTake9;
-    public String hotTake10;
+    private String username;
+    private List<String> hotTakes;
 
     public String getUsername() {
         return username;
@@ -28,83 +19,11 @@ public class HotTakes {
         this.username = username;
     }
 
-    public String getHotTake1() {
-        return hotTake1;
+    public List<String> getHotTakes() {
+        return hotTakes;
     }
 
-    public void setHotTake1(String hotTake1) {
-        this.hotTake1 = hotTake1;
-    }
-
-    public String getHotTake2() {
-        return hotTake2;
-    }
-
-    public void setHotTake2(String hotTake2) {
-        this.hotTake2 = hotTake2;
-    }
-
-    public String getHotTake3() {
-        return hotTake3;
-    }
-
-    public void setHotTake3(String hotTake3) {
-        this.hotTake3 = hotTake3;
-    }
-
-    public String getHotTake4() {
-        return hotTake4;
-    }
-
-    public void setHotTake4(String hotTake4) {
-        this.hotTake4 = hotTake4;
-    }
-
-    public String getHotTake5() {
-        return hotTake5;
-    }
-
-    public void setHotTake5(String hotTake5) {
-        this.hotTake5 = hotTake5;
-    }
-
-    public String getHotTake6() {
-        return hotTake6;
-    }
-
-    public void setHotTake6(String hotTake6) {
-        this.hotTake6 = hotTake6;
-    }
-
-    public String getHotTake7() {
-        return hotTake7;
-    }
-
-    public void setHotTake7(String hotTake7) {
-        this.hotTake7 = hotTake7;
-    }
-
-    public String getHotTake8() {
-        return hotTake8;
-    }
-
-    public void setHotTake8(String hotTake8) {
-        this.hotTake8 = hotTake8;
-    }
-
-    public String getHotTake9() {
-        return hotTake9;
-    }
-
-    public void setHotTake9(String hotTake9) {
-        this.hotTake9 = hotTake9;
-    }
-
-    public String getHotTake10() {
-        return hotTake10;
-    }
-
-    public void setHotTake10(String hotTake10) {
-        this.hotTake10 = hotTake10;
+    public void setHotTakes(List<String> hotTakes) {
+        this.hotTakes = hotTakes;
     }
 }
