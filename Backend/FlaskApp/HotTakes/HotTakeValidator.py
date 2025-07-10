275183889c824ccd05d14973602ac96ca094d588
russellchao <russchaoman@gmail.com>
2025-07-10 16:15:48 -0400

    Working on AI Model that validates a hot take.

--- Backend/FlaskApp/HotTakes/HotTakeValidator.py
+++ Backend/FlaskApp/HotTakes/HotTakeValidator.py
@@ -1,11 +1,34 @@
 def validate_hot_take(hotTake, listOfHotTakes):
+
+    ##### SECTION 1: BASIC CONDITIONS FOR INVALIDATION #####
+    # 1. If there already exists 10 hot takes
+    # 2. If the hot take already exists in the currentl list of hot takes
+
+    # TODO: 
+    # For the second condition, set every letter to lowercase, check for extra punctuation, check for contractions
+    # in the incoming take and each hot take in the current list of hot takes
     
-    # If the length of the hot takes list is already 10
     if len(listOfHotTakes) >= 10:
         return "This hot take is invalid, you already have 10 hot takes"
     
-    # If the hot take is already in the list of hot takes
     if hotTake in listOfHotTakes:
         return "This hot take is invalid, you already have this hot take"
+    
+    
+
+    ##### SECTION 2: CHECK FOR CONTRADICTIONS IN THE INCOMING TAKE AGAINST EXISTING TAKES #####
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return "This hot take is valid"
