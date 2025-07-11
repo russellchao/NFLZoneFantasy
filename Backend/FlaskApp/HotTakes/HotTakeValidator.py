68eaeb98b6bc7c41069cff5e70ccb5e0b3103692
russellchao <russchaoman@gmail.com>
2025-07-10 21:40:23 -0400

    Implemented a basic contradiction invalidator in the HotTakeValidator.py file

--- Backend/FlaskApp/HotTakes/HotTakeValidator.py
+++ Backend/FlaskApp/HotTakes/HotTakeValidator.py
@@ -1,6 +1,9 @@
-def validate_hot_take(hotTake, listOfHotTakes):
+import re
 
-    ##### SECTION 1: BASIC CONDITIONS FOR INVALIDATION #####
+
+def basic_invalidator(hotTake, listOfHotTakes):
+
+    ##### BASIC CONDITIONS FOR INVALIDATION #####
     # 1. If there already exists 10 hot takes
     # 2. If the hot take already exists in the currentl list of hot takes
 
@@ -14,9 +17,59 @@ def validate_hot_take(hotTake, listOfHotTakes):
     if hotTake in listOfHotTakes:
         return "This hot take is invalid, you already have this hot take"
     
+    return "This hot take passes the basic invalidation test"
+
+
+
+def extract_team_and_action(hotTake):
+    # Very basic extractor, this can be expanded 
+    patterns = [
+        (r'(.*?) will win the (AFC|NFC|Super Bowl|[A-Z]{2,} [A-Z][a-z]+)', 'win'),
+        (r'(.*?) won\'t win the (AFC|NFC|Super Bowl|[A-Z]{2,} [A-Z][a-z]+)', 'not_win'),
+        (r'(.*?) will make the playoffs', 'make_playoffs'),
+        (r'(.*?) will miss the playoffs', 'miss_playoffs'),
+    ]
+
+    for pattern, action in patterns:
+        match = re.search(pattern, hotTake, re.IGNORECASE)
+
+        if match:
+            team = match.group(1).strip()
+            return (team.lower(), action)
+        
+    return (None, None)
+
+
+
+def is_contradictory(hotTake, listOfHotTakes):
+    team1, action1 = extract_team_and_action(hotTake)
+    if not team1 or not action1:
+        return False  # Not analyzable
     
+    for take in listOfHotTakes:
+        team2, action2 = extract_team_and_action(take)
+        if not team2 or not action2:
+            continue
 
-    ##### SECTION 2: CHECK FOR CONTRADICTIONS IN THE INCOMING TAKE AGAINST EXISTING TAKES #####
+        if team1 == team2:
+            if (action1 == 'make_playoffs' and action2 == 'miss_playoffs') or \
+               (action1 == 'miss_playoffs' and action2 == 'make_playoffs') or \
+               (action1 == 'win' and action2 == 'not_win') or \
+               (action1 == 'not_win' and action2 == 'win'):
+                return True
+
+        # Mutually exclusive titles
+        if action1 == 'win' and action2 == 'win' and team1 != team2:
+            if 'super bowl' in hotTake.lower() and 'super bowl' in take.lower():
+                return True
+            if 'afc' in hotTake.lower() and 'afc' in take.lower():
+                return True
+            if 'nfc' in hotTake.lower() and 'nfc' in take.lower():
+                return True
+            if 'division' in hotTake.lower() and 'division' in take.lower():
+                return True
+
+    return False
 
 
 
@@ -24,9 +77,15 @@ def validate_hot_take(hotTake, listOfHotTakes):
 
 
 
+def validate_hot_take(hotTake, listOfHotTakes):
 
+    # 1. Basic invalidator
+    result = basic_invalidator(hotTake, listOfHotTakes)
+    if result != "This hot take passes the basic invalidation test": return result
 
-
+    # 2. Contradiction invalidator
+    if is_contradictory(hotTake, listOfHotTakes):
+        return "This hot take is invalid, it contradicts an existing hot take"
 
 
 
